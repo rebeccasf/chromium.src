06e345e6ba8c8954f1302a1299439fb2b220cdcd
Rebecca Chang Swee Fun <rebecca.chang@starfivetech.com>
2022-09-20 22:37:09 -0400

    tools: clang: script: add atomic build flag for native build
    Signed-off-by: Rebecca Chang Swee Fun <rebecca.chang@starfivetech.com>

--- tools/clang/scripts/build.py
+++ tools/clang/scripts/build.py
@@ -663,6 +663,8 @@ def main():
       '-DLLVM_ENABLE_CURL=OFF',
       # Build libclang.a as well as libclang.so
       '-DLIBCLANG_BUILD_STATIC=ON',
+      # StarFive: Required for native RISCV build
+      '-DSANITIZER_COMMON_LINK_LIBS=-latomic',
   ]
 
   if sys.platform == 'darwin':
